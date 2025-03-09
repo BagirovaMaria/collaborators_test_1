e1f500e5d8ad94a05814e9ee323205d13d4b0e70
EvgeniiBuller <buller.1@gmx.de>
2025-03-09 14:13:29 +0100

    add 2

---
+++ evgenii2.js
@@ -0,0 +1,14 @@
+function groupByParity(arr) {
+    const result = { even: [], odd: [] };
+
+    for (const num of arr) {
+        if (num % 2 === 0) {
+            result.even.push(num);
+        } else {
+            result.odd.push(num);
+        }
+    }
+    return result;
+}
+
+console.log(groupByParity([1, 2, 3, 4, 5, 6]));
