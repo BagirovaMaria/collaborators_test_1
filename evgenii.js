c71bb8619d19849022e4e2b78cc452b439c0ec74
EvgeniiBuller <buller.1@gmx.de>
2025-03-09 10:52:53 +0100

    add group-by-parity

--- evgenii.js
+++ evgenii.js
@@ -1,25 +1,3 @@
-function removeDuplicates(arr) {
-    return [...new Set(arr)];
-}
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
-
-function mostFrequent(arr) {
-    const frequencyMap  ={};
-    let mostFrequentElement;
-    let maxFrequency = 0;
-
-    for (const num of arr) {
-        frequencyMap[num] = (frequencyMap[num] || 0) + 1;
-        if (frequencyMap[num] > maxFrequency) {
-            mostFrequentElement = num;
-            maxFrequency = frequencyMap[num];
-        }
-    }
-    return mostFrequentElement;
-}
-
-console.log(mostFrequent([1, 2, 2, 3, 3, 3, 4]));
-
 function groupByParity(arr) {
     const result = { even: [], odd: [] };
 
@@ -31,32 +9,6 @@ function groupByParity(arr) {
         }
     }
     return result;
-}console.log(groupByParity([1, 2, 3, 4, 5, 6]));
-
-function isPrime(num) {
-    if (num <= 1) return false;
-    for (let i = 2; i <= Math.sqrt(num); i++)
-        if (num % i === 0) return false;
 }
 
-function filterPrimes(arr) {
-    return arr.filter(isPrime);
-}
-
-console.log(filterPrimes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
-
-function countDivisors(num) {
-    let count = 0;
-    for (let i = 1; i <= num; i++) {
-        if (num % i === 0) {
-            count++;
-        }
-    }
-    return count;
-}
-
-function sortByDivisors(arr) {
-    return arr.sort((a, b) => countDivisors(a) - countDivisors(b));
-}
-
-console.log(sortByDivisors([10, 15, 8, 3, 6]));
+console.log(groupByParity([1, 2, 3, 4, 5, 6]));
